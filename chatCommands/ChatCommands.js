fafe5e423a6bbddcd2fdd56e0e6ae1deddb3d94d
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2022-04-17 01:00:30 +0200

    Delete chatCommands directory

--- chatCommands/ChatCommands.js
+++
@@ -1,8 +0,0 @@
-const Client = require("./Base/Client.js");
-
-const client = new Client({ config: "./config.json" });
-
-client.loadCommands();
-client.loadEvents();
-
-client.log("Client loaded");
